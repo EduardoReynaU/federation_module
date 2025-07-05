3c67314230a152d982afe37e55c844892477fdc9
EduardoReynaU <eduardojuan.reyna@unmsm.edu.pe>
2025-07-05 16:15:54 -0500

    asdkfjaks

--- gateway/index.js
+++ gateway/index.js
@@ -3,8 +3,8 @@ import { ApolloGateway } from '@apollo/gateway';
 
 const gateway = new ApolloGateway({
   serviceList: [
-    { name: 'auten', url: 'https://auten-service-356996092654.us-central1.run.app/graphql' },
-    { name: 'dashboard', url: 'https://dashboard-service-356996092654.us-central1.run.app/graphql' }
+    { name: 'auten', url: 'https://auten-service-356996092654.us-central1.run.app' },
+    { name: 'dashboard', url: 'https://dashboard-service-356996092654.us-central1.run.app' }
     // Aquí irán más servicios en el futuro
   ]
 });
